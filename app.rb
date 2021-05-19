64fe605c8fa476440025c81c21285e2718a73ff5
ekjones1993 <emilykjones1993@gmail.com>
2021-05-19 15:26:25 +0100

    12 - find method

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks/:id/edit' do
-    @bookmark_id = params[:id]
+    @bookmark = Bookmark.find(id: params[:id])
     erb :'bookmarks/edit'
   end
 
